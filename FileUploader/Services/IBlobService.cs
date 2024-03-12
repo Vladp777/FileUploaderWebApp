4d196fdb2b9127e9f2d6788c68499e1b87d164a7
Vlad <vladp.ua777@gmail.com>
2024-03-12 15:14:04 +0200

    some fix

--- FileUploader/Services/IBlobService.cs
+++ FileUploader/Services/IBlobService.cs
@@ -5,6 +5,8 @@ namespace FileUploader.Services
     public interface IBlobService
     {
         Task UploadFileAsync(IBrowserFile file, string email);
+        Task<string> UploadFileAsync(string fileName, Stream stream, string type);
+
         Task UploadFileAsync(IBrowserFile file, Stream stream, string email);
     }
 }
